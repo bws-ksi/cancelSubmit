86bddd41dc04dde88e807e393d0d4af198e2d6c8
bws.ksi <bws.ksi@gmail.com>
2022-05-19 23:57:54 +0300

    update

--- src/script.js
+++ src/script.js
@@ -1,28 +1,18 @@
 'use strict';
 // объявление переменных
 const submit = document.querySelector('.submit');
-const reset = document.querySelector('.reset');
+// const reset = document.querySelector('.reset');
 const email = document.querySelector('.email_input').value;
-const genderRadio = document.getElementsByName('gender');
+// const genderRadio = document.getElementsByName('gender');
+const radioInput = document.forms.main.gender;
+const allInputFields = document.querySelectorAll('.input');
+const pass = document.forms.main.pass.value;
+const confirmPass = document.forms.main.confirm_pass.value;
+console.log(allInputFields);
 
-function checkFields(event) {
-  event.preventDefault();
-  //проверка
 
-
-  console.log(userData);
-  localStorage.setItem(`${userData.lName}`, JSON.stringify(userData));
-}
-let choiceGender;
-for (let i = 0; i < genderRadio.length; i++) {  
-  if (genderRadio[i].checked) {
-    choiceGender = genderRadio[i].value;
-  }
-  console.log(choiceGender);
-}
-  
-
-// создание массива
+console.log(radioInput);
+const collectionInput = Array.from(document.getElementsByClassName('input_field'));
 class User {
   constructor(fName, lName, age, email) {
     this.fName = fName;
@@ -32,17 +22,81 @@ class User {
   }
 }
 const userData = new User();
-
-// сбор данных по полям
-const collectionInput = Array.from(document.getElementsByClassName('input_field'));
-
-collectionInput.forEach((elem) => {
-  userData[elem.name] = elem.value;
-  userData.gender = choiceGender;
+//============================================================
+//============================================================
+function checkFields() {
+  event.preventDefault();
+  // проверяю есть ли пустые поля в .input и RadioInput (проверяю каждый элемент)
+  //
+  //проверка
+allInputFields.forEach((input) => {
+  if(input.value === '') {
+    input.parentNode.classList.add('error')
+  } else {input.parentNode.classList.remove('error')}
 })
-console.log(userData);
+radioInput.forEach((input) => {
+  if(!input.checked) {
+    input.parentNode.classList.add('error')
+  } else {input.parentNode.classList.remove('error')}
+}) 
+  //
+  //
+  //
+  //
+  //проверка электронной почты
+  //
+  // проверка пароля
+  //
+  //когда все верно указано выполняется функция collectToSend
+
+  collectToSend();
+  
+}
+
+
+function collectToSend(event) {
+  
+  let choiceGender;
+
+  for (let i = 0; i < radioInput.length; i++) {
+    if (radioInput[i].checked) {
+      choiceGender = radioInput[i].value;
+    }
+  }
+
+
+  // for (let i = 0; i < genderRadio.length; i++) {
+  //   if (genderRadio[i].checked) {
+  //     choiceGender = genderRadio[i].value;
+  //   }
+  // }
+  collectionInput.forEach((elem) => {
+    userData[elem.name] = elem.value;
+    userData.gender = choiceGender;
+  })
+  // console.log(userData);
+  localStorage.setItem(`${userData.lName}`, JSON.stringify(userData));
+  // document.getElementById('form').reset()
+}
+
+function validateEmail(email){
+  const regex =  /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/i
+  return regex.test(String(email))
+}
+function comparisonPasswords (pass, confirmPass) {
+  if(String(pass) === String(confirmPass)) {
+    return true;
+  } return false;
+}
+
+
+// console.log(userData);
 // событие на "отправить"
 submit.addEventListener('click', checkFields);
 
 // событие на "сбросить"
-reset.addEventListener('click', document.getElementById('form').reset())
+// reset.addEventListener('click', document.getElementById('form').reset())
+
+
+
+
